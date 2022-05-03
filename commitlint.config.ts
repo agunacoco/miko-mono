215448d15b32f18a3c380bff8405913ff7ff7f5d
Aio <AioDev.JS@gmail.com>
2022-05-03 19:53:47 +0900

    UPDATE(root): update commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,7 +5,7 @@ const Configuration: UserConfig = {
    * Resolve and load @commitlint/config-conventional from node_modules.
    * Referenced packages must be installed
    */
-  // extends: ['@commitlint/config-conventional'],
+  extends: ['@commitlint/config-conventional'],
   /*
    * Resolve and load conventional-changelog-atom from node_modules.
    * Referenced packages must be installed
@@ -20,13 +20,16 @@ const Configuration: UserConfig = {
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
   rules: {
-    'type-enum': [2, 'always', ['ADD', 'MERGE', 'UPDATE', 'FIX', 'PU', 'DEPLOY', 'RF', 'DOCS']],
+    'type-enum': [2, 'always', ['FEAT', 'MERGE', 'UPDATE', 'FIX', 'PU', 'DEPLOY', 'RF', 'DOCS', 'ETC', 'STYLE', 'REVERT']],
     'type-case': [2, 'always', 'upper-case'],
+    'scope-enum': [2, 'always', ['types', 'utils', 'view', 'stream', 'etc', 'root']],
+    'scope-case': [2, 'always', 'lower-case'],
+    'scope-empty': [2, 'never'],
   },
   /*
    * Functions that return true if commitlint should ignore the given message.
    */
-  ignores: [(commit) => commit === ''],
+  ignores: [commit => commit === ''],
   /*
    * Whether commitlint uses the default ignore rules.
    */
