121f8a3ae82b3cb7b9028105e2950645dd235b22
GU NA HYUN <80106663+agunacoco@users.noreply.github.com>
2022-05-06 16:06:35 +0900

    UPDATE(view): enter page ticket, add image

--- packages/miko-viewer/src/components/live/DynamicEnter.tsx
+++ packages/miko-viewer/src/components/live/DynamicEnter.tsx
@@ -1,54 +1,19 @@
-import { Box, Button, createIcon, Flex, Grid, GridItem, Heading, HStack, Icon, IconButton, Input, Radio, RadioGroup, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, HStack, Icon, Input, Radio, RadioGroup, Stack, Text, useColorModeValue } from '@chakra-ui/react';
 import { FcLock } from '@react-icons/all-files/fc/FcLock';
 import { MdPublic } from '@react-icons/all-files/md/MdPublic';
-import { IMAGE_DOMAIN, USER_TICKET_COOKIE } from '@src/const';
+import { USER_TICKET_COOKIE } from '@src/const';
 import { convertDate, setCookie } from '@src/helper';
 import { curUserTicketState, enterRoomIdState, enterTicketDataState } from '@src/state/recoil';
 import { useUser } from '@src/state/swr';
 import { nanoid } from 'nanoid';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React, { FC, useEffect, useMemo, useState } from 'react';
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
-const PlayIcon = createIcon({
-  displayName: 'PlayIcon',
-  viewBox: '0 0 58 58',
-  d: 'M28.9999 0.562988C13.3196 0.562988 0.562378 13.3202 0.562378 29.0005C0.562378 44.6808 13.3196 57.438 28.9999 57.438C44.6801 57.438 57.4374 44.6808 57.4374 29.0005C57.4374 13.3202 44.6801 0.562988 28.9999 0.562988ZM39.2223 30.272L23.5749 39.7247C23.3506 39.8591 23.0946 39.9314 22.8332 39.9342C22.5717 39.9369 22.3142 39.8701 22.0871 39.7406C21.86 39.611 21.6715 39.4234 21.5408 39.1969C21.4102 38.9705 21.3421 38.7133 21.3436 38.4519V19.5491C21.3421 19.2877 21.4102 19.0305 21.5408 18.8041C21.6715 18.5776 21.86 18.3899 22.0871 18.2604C22.3142 18.1308 22.5717 18.064 22.8332 18.0668C23.0946 18.0696 23.3506 18.1419 23.5749 18.2763L39.2223 27.729C39.4404 27.8619 39.6207 28.0486 39.7458 28.2713C39.8709 28.494 39.9366 28.7451 39.9366 29.0005C39.9366 29.2559 39.8709 29.507 39.7458 29.7297C39.6207 29.9523 39.4404 30.1391 39.2223 30.272Z',
-});
-
-const EnterRoomBtn = () => {
-  const router = useRouter();
-  const curUserTicket = useRecoilValue(curUserTicketState);
-  console.log('enter room btn', curUserTicket);
-  const enterHandler = () => {
-    setCookie(USER_TICKET_COOKIE, curUserTicket.id + '', 6);
-    router.push('/live/viewing', '', { shallow: false });
-  };
-
-  return (
-    <IconButton
-      onClick={enterHandler}
-      zIndex={1}
-      aria-label={'Play Button'}
-      variant={'ghost'}
-      _hover={{
-        bg: 'transparent',
-      }}
-      icon={<PlayIcon w={12} h={12} />}
-      size={'lg'}
-      color={'red.400'}
-      position={'absolute'}
-      left={'50%'}
-      top={'50%'}
-      transform={'translateX(-50%) translateY(-50%)'}
-    />
-  );
-};
-
 const PrivateRoomIdInput = () => {
   const [roomId, setRoomId] = useRecoilState(enterRoomIdState);
   const router = useRouter();
+
   useEffect(() => {
     const queryRoomId = router.query?.roomId;
     if (queryRoomId) {
@@ -62,8 +27,25 @@ const PrivateRoomIdInput = () => {
 
   return (
     <HStack>
-      <Input value={roomId} isInvalid={roomId?.length !== 21} contentEditable="false" onChange={e => setRoomId(e.target.value)} />
-      <Button colorScheme="green" onClick={createRoomIdHandler}>
+      <Input
+        errorBorderColor="white"
+        focusBorderColor="orange.400"
+        bgColor="purple.900"
+        placeholder="Make a room ID"
+        value={roomId}
+        isInvalid={roomId?.length !== 21}
+        contentEditable="false"
+        onChange={e => setRoomId(e.target.value)}
+      />
+      <Button
+        bgColor="orange"
+        _hover={{ bg: 'orange.400' }}
+        _active={{
+          bg: 'orange.400',
+          transform: 'scale(0.98)',
+        }}
+        onClick={createRoomIdHandler}
+      >
         ID 生成
       </Button>
     </HStack>
@@ -77,7 +59,7 @@ const RoomSelect = () => {
 
   const radioChangeHandler = (value: string) => {
     if (value === 'public') {
-      setEnterRoomId(undefined);
+      setEnterRoomId('');
     }
     setRadioValue(value);
   };
@@ -86,27 +68,29 @@ const RoomSelect = () => {
     <Box>
       <RadioGroup value={radioValue} onChange={radioChangeHandler}>
         <Stack spacing={5} direction="row">
-          <Radio colorScheme="green" value="private">
+          <Radio colorScheme="orange" value="private">
             <HStack spacing={1}>
-              <Text>プライベート</Text>
+              <Text color="orange" fontWeight="bold">
+                プライベート
+              </Text>
               {radioValue == 'private' && <Icon boxSize={5} as={FcLock} />}
             </HStack>
           </Radio>
           <Radio colorScheme="blue" value="public">
             <HStack spacing={1}>
-              <Text>ランダム</Text>
-              {radioValue == 'public' && <Icon boxSize={5} as={MdPublic} color="blue.500" />}
+              <Text color="cyan.300" fontWeight="bold">
+                ランダム
+              </Text>
+              {radioValue == 'public' && <Icon boxSize={5} as={MdPublic} color="cyan.400" />}
             </HStack>
           </Radio>
         </Stack>
       </RadioGroup>
       <Flex h="60px" w="full" alignItems="center">
         {isPublicRoom ? (
-          <Box>
-            <Text fontSize="lg" fontWeight="510" color="blue.500">
-              ランダムのルームに入場します。
-            </Text>
-          </Box>
+          <Text fontSize="lg" fontWeight="bold" color="cyan.300">
+            ランダムのルームに入場します。
+          </Text>
         ) : (
           <PrivateRoomIdInput />
         )}
@@ -116,10 +100,14 @@ const RoomSelect = () => {
 };
 
 const CurEnterInfo: FC = () => {
+  const topBg = useColorModeValue('gray.100', 'gray.700');
+  const [ticketMotion, setTicketMotion] = useState(false);
+
   const router = useRouter();
   useUser();
   const { concert } = useRecoilValue(curUserTicketState);
   const curTicket = useRecoilValue(enterTicketDataState);
+  const [roomId, setRoomId] = useRecoilState(enterRoomIdState);
 
   useEffect(() => {
     if (router.isReady) {
@@ -132,88 +120,89 @@ const CurEnterInfo: FC = () => {
 
   if (!curTicket || !concert) return <Box>no ticket</Box>;
 
-  // const { ticket, concert } = curUserTicket;
-
   const startDate = convertDate(curTicket.concertStartDate, 'YMDHM'); // 티켓 시작날
   const endDate = convertDate(curTicket.concertEndDate, 'YMDHM'); // 티켓 끝나는 날
-  const archiveEndTime = convertDate(curTicket.archiveEndTime, 'YMDHM'); // 티켓 끝나는 날
 
-  const [transHover, setTransHover] = useState(false);
+  const curUserTicket = useRecoilValue(curUserTicketState);
+  console.log('enter room btn', curUserTicket);
+  const enterHandler = () => {
+    setCookie(USER_TICKET_COOKIE, curUserTicket.id + '', 6);
+    router.push('/live/viewing', '', { shallow: false });
+  };
+
+  const onClick = () => {
+    setTicketMotion(true);
+    setTimeout(function () {
+      enterHandler();
+    }, 900);
+  };
+
   return (
-    <Box w="full" px={20}>
-      <Stack align={'center'} spacing={{ base: 8, md: 10 }} py={{ base: 4, md: 6 }} direction={{ base: 'column', md: 'row' }}>
-        <Stack flex={1} spacing={{ base: 5, md: 5 }}>
-          <Box>
-            <Heading fontWeight={600} fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
-              {concert.title}
-            </Heading>
-            <Heading color={'red.400'} fontSize={{ base: 'lg', sm: 'xl', lg: '2xl' }} my={3}>
-              {concert.artist}
-            </Heading>
-          </Box>
-          <Grid templateRows="repeat(1, 1fr)" templateColumns="repeat(6, 1fr)">
-            <GridItem rowSpan={1} colSpan={2}>
-              <Heading as="h5" size="sm" my={2}>
-                公演期間
-              </Heading>
-              <Heading as="h5" size="sm" my={2}>
-                公演時間
-              </Heading>
-              <Heading as="h5" size="sm" my={2}>
-                アーカイブ視聴期間
-              </Heading>
-            </GridItem>
-            <GridItem rowSpan={1} colSpan={4}>
-              <Heading as="h5" size="sm" my={2}>
-                {startDate} ~ {endDate}時
-              </Heading>
-              <Heading as="h5" size="sm" my={2}>
-                {curTicket.runningTime}分
-              </Heading>
-              <Heading as="h5" size="sm" my={2}>
-                {archiveEndTime}まで
-              </Heading>
-            </GridItem>
-          </Grid>
-          <Text color={'gray.500'} className="word" maxW="65vh">
-            {concert.content}
+    <Flex className="cardWrap" p={20}>
+      <Stack
+        shadow="xl"
+        className="card cardLeft"
+        bgPosition="40% 70%"
+        bgRepeat="no-repeat"
+        bgSize="150%"
+        bgImage="/image/ticketbg.jpg"
+        spacing={4}
+        p={12}
+        flex="0.7"
+        color="white"
+      >
+        <HStack spacing={5}>
+          <Text fontSize="5xl" fontWeight="bold">
+            {concert.title}
           </Text>
-          <Stack spacing={{ base: 4, sm: 6 }} direction={{ base: 'column', sm: 'row' }}>
-            <RoomSelect />
-            {/* <Button rounded={'full'} size={'lg'} fontWeight={'normal'} px={6} colorScheme={'red'} bg={'red.400'} _hover={{ bg: 'red.500' }}>
-              Get started
-            </Button> */}
-          </Stack>
-        </Stack>
-        <Flex direction="column" flex={1} justify={'center'} align={'start'} position={'relative'} w={'full'}>
-          <Box
-            position={'relative'}
-            height={'450px'}
-            rounded={'2xl'}
-            boxShadow={'2xl'}
-            width={'full'}
-            overflow={'hidden'}
-            _hover={{
-              bg: 'tomato',
-              boxShadow: 'xl',
-              transition: 'all .2s ease',
-              transform: 'scale(1.04)',
-            }}
-          >
-            <EnterRoomBtn />
-            <Image
-              src={IMAGE_DOMAIN + concert.coverImage}
-              placeholder="blur"
-              blurDataURL="/image/defaultImage.png"
-              quality={70}
-              layout="fill"
-              objectFit="cover"
-              alt={`${concert.title} image`}
-              style={{ borderRadius: '12px' }}
-            />
-          </Box>
-        </Flex>
+          <Text color="yellow.400" pt={6} fontFamily="body" fontWeight="semibold" fontSize="xl">
+            {concert.artist}
+          </Text>
+        </HStack>
+        <Text className="word" color="gray.200">
+          {concert.detail}
+        </Text>
+        <Flex w="full" borderTopWidth="1px" h="3px" borderTopColor={topBg} />
+        <Text fontSize="2xl" fontWeight="semibold">
+          {startDate} ~ {endDate}
+        </Text>
+        <RoomSelect />
       </Stack>
+
+      <Stack
+        transform={ticketMotion && 'translateX(100px) rotateZ(20deg)'}
+        transitionDuration="1s"
+        onClick={onClick}
+        shadow="xl"
+        className="card  cardRight"
+        color="white"
+        flex="0.3"
+        p={7}
+        justify="center"
+        align="start"
+        bg="linear-gradient( to top, purple, pink )"
+      >
+        {/* <Box transform="rotate(90deg)"> */}
+        <Text color="cyan" fontSize="xl" fontWeight="semibold">
+          {concert.artist}
+        </Text>
+        <Text lineHeight={10} fontSize="5xl" fontWeight="extrabold">
+          {concert.title}
+        </Text>
+        <Text color="yellow" fontSize="2xl" fontWeight="extrabold">
+          {startDate}
+        </Text>
+        <Box py={5}>
+          <Text w="330px" color="cyan.300" fontSize="md">
+            {roomId ? 'PRIVATE ROOM ID' : 'PUBLIC ROOM'}
+          </Text>
+          <Text w="330px" fontSize="2xl" fontWeight="bold">
+            {roomId}
+          </Text>
+        </Box>
+        <Box className="barcode"></Box>
+      </Stack>
+
       <style>
         {`
         .word {
@@ -222,12 +211,133 @@ const CurEnterInfo: FC = () => {
         white-space: normal;
         word-wrap: break-word;
         display: -webkit-box;
-        -webkit-line-clamp: 3 ;
+        -webkit-line-clamp: 2;
         -webkit-box-orient: vertical;
+        }
+
+        .cardLeft {
+          border-right: .2em dashed #fff;
+          border-top-left-radius: 20px;
+          border-bottom-left-radius: 20px;
+        }
+
+        .cardRight {
+          border-left: .2em dashed #fff;
+          border-top-right-radius: 20px;
+          border-bottom-right-radius: 20px;
+        } 
+
+        .barcode {
+          height: 4em;
+          width: 0;
+          margin: 1.2em 0 0 .8em;
+          box-shadow: 1px 0 0 1px #343434,
+          5px 0 0 1px #343434,
+          10px 0 0 1px #343434,
+          11px 0 0 1px #343434,
+          15px 0 0 1px #343434,
+          18px 0 0 1px #343434,
+          22px 0 0 1px #343434,
+          23px 0 0 1px #343434,
+          26px 0 0 1px #343434,
+          30px 0 0 1px #343434,
+          35px 0 0 1px #343434,
+          37px 0 0 1px #343434,
+          41px 0 0 1px #343434,
+          44px 0 0 1px #343434,
+          47px 0 0 1px #343434,
+          51px 0 0 1px #343434,
+          56px 0 0 1px #343434,
+          59px 0 0 1px #343434,
+          64px 0 0 1px #343434,
+          68px 0 0 1px #343434,
+          72px 0 0 1px #343434,
+          74px 0 0 1px #343434,
+          77px 0 0 1px #343434,
+          81px 0 0 1px #343434,
+          85px 0 0 1px #343434,
+          89px 0 0 1px #343434,
+          91px 0 0 1px #343434,
+          94px 0 0 1px #343434,
+          97px 0 0 1px #343434,
+          100px 0 0 1px #343434,
+          105px 0 0 1px #343434,
+          110px 0 0 1px #343434,
+          111px 0 0 1px #343434,
+          115px 0 0 1px #343434,
+          118px 0 0 1px #343434,
+          122px 0 0 1px #343434,
+          123px 0 0 1px #343434,
+          126px 0 0 1px #343434,
+          130px 0 0 1px #343434,
+          135px 0 0 1px #343434,
+          137px 0 0 1px #343434,
+          141px 0 0 1px #343434,
+          144px 0 0 1px #343434,
+          147px 0 0 1px #343434,
+          151px 0 0 1px #343434,
+          156px 0 0 1px #343434,
+          159px 0 0 1px #343434,
+          164px 0 0 1px #343434,
+          168px 0 0 1px #343434,
+          172px 0 0 1px #343434,
+          174px 0 0 1px #343434,
+          177px 0 0 1px #343434,
+          181px 0 0 1px #343434,
+          185px 0 0 1px #343434,
+          189px 0 0 1px #343434,
+          191px 0 0 1px #343434,
+          194px 0 0 1px #343434,
+          197px 0 0 1px #343434,
+          200px 0 0 1px #343434,
+          205px 0 0 1px #343434,
+          210px 0 0 1px #343434,
+          211px 0 0 1px #343434,
+          215px 0 0 1px #343434,
+          218px 0 0 1px #343434,
+          222px 0 0 1px #343434,
+          223px 0 0 1px #343434,
+          226px 0 0 1px #343434,
+          230px 0 0 1px #343434,
+          235px 0 0 1px #343434,
+          237px 0 0 1px #343434,
+          241px 0 0 1px #343434,
+          244px 0 0 1px #343434,
+          247px 0 0 1px #343434,
+          251px 0 0 1px #343434,
+          256px 0 0 1px #343434,
+          259px 0 0 1px #343434,
+          264px 0 0 1px #343434,
+          268px 0 0 1px #343434,
+          272px 0 0 1px #343434,
+          274px 0 0 1px #343434,
+          277px 0 0 1px #343434,
+          281px 0 0 1px #343434,
+          285px 0 0 1px #343434,
+          289px 0 0 1px #343434,
+          291px 0 0 1px #343434,
+          294px 0 0 1px #343434,
+          297px 0 0 1px #343434,
+          300px 0 0 1px #343434,
+          305px 0 0 1px #343434,
+          310px 0 0 1px #343434,
+          311px 0 0 1px #343434,
+          315px 0 0 1px #343434,
+          318px 0 0 1px #343434,
+          322px 0 0 1px #343434,
+          323px 0 0 1px #343434,
+          326px 0 0 1px #343434,
+          330px 0 0 1px #343434,
+          335px 0 0 1px #343434,
+          337px 0 0 1px #343434,
+          341px 0 0 1px #343434,
+          344px 0 0 1px #343434,
+          347px 0 0 1px #343434,
+          351px 0 0 1px #343434;
         }
         `}
       </style>
-    </Box>
+    </Flex>
   );
 };
 
