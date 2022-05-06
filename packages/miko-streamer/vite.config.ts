585834bde4d9ef135e392cfb6806d13795b27c5c
Aio <AioDev.JS@gmail.com>
2022-05-06 10:08:55 +0900

    UPDATE(stream): update package json

--- packages/miko-streamer/vite.config.ts
+++ packages/miko-streamer/vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+// ts paths Error
+// 패케지 명과 ts path를 동일하게해서 오류가 나와야하는 상황에서 에러가 안 나오게 됨
+// ts paths는 bundler 시스템에 의존해서 ts 이외에도 추가 설정해줘야만함.
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -16,7 +20,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@src': path.resolve(__dirname, './src'),
-      '@miko/share-types': path.resolve(__dirname, '../shareTypes'),
+      // '@miko/share-types': path.resolve(__dirname, '../shareTypes'),
     },
   },
 });
